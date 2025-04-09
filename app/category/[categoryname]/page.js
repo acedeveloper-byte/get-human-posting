7a6500e424f144fbda76283ecb7a3289117d2654
acedeveloper-byte <mayank-kumarace@gmail.com>
2025-04-09 14:14:04 +0530

    category page create

--- app/category/[categoryname]/page.js
+++ app/category/[categoryname]/page.js
@@ -1,11 +1,24 @@
+'use client'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import CategoryDetails from '@/components/CategoryDetails';
+
 
 const page = () => {
     return (
-        <div>
+        < >
+            <Header />
+            <div className='penci-single-wrapper'>
 
-        </div>
+                <CategoryDetails />
+            </div>
+            <Footer />
+        </>
+
     )
 }
+
 
 export default page
