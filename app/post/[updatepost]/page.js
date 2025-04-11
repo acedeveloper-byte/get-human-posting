b72834a501fed8274cb923dcfa0c7fa9246eeada
acedeveloper-byte <mayank-kumarace@gmail.com>
2025-04-11 13:28:42 +0530

    adde change

--- app/post/[updatepost]/page.js
+++ app/post/[updatepost]/page.js
@@ -94,39 +94,38 @@ const page = () => {
                                     <Form.Label>Current Category {data_blog.category}</Form.Label>
                                     <Form.Select {...formik.getFieldProps("category")}>
                                         {[
-                                            { "value": "", "label": "Choose Categories" },
-                                            { "value": "413", "label": "Lifestyle" },
-                                            { "value": "413", "label": "Latest" },
-                                            { "value": "413", "label": "Featured" },
-                                            { "value": "413", "label": "Tech" },
-                                            { "value": "413", "label": "Healthy" },
-                                            { "value": "413", "label": "Fashion" },
-                                            { "value": "413", "label": "Business" },
-                                            { "value": "410", "label": "Design" },
-                                            { "value": "7", "label": "Domain Names" },
-                                            { "value": "426", "label": "Education" },
-                                            { "value": "411", "label": "Entertainment" },
-                                            { "value": "416", "label": "Home & Lifestyle" },
-                                            { "value": "425", "label": "Marketing" },
-                                            { "value": "6", "label": "Digital Marketing", "indent": true },
-                                            { "value": "5", "label": "SEO", "indent": true },
-                                            { "value": "4", "label": "Social Media", "indent": true },
-                                            { "value": "412", "label": "Music" },
-                                            { "value": "1", "label": "Other" },
-                                            { "value": "437", "label": "Politics" },
-                                            { "value": "436", "label": "Real Estate" },
-                                            { "value": "415", "label": "Sports" },
-                                            { "value": "414", "label": "Technology" },
-                                            { "value": "418", "label": "Travel" },
-                                            { "value": "299", "label": "Video" },
-                                            { "value": "8", "label": "Web Hosting" },
-                                            { "value": "9", "label": "Web Security" },
-                                            { "value": "417", "label": "Wellness" },
-                                            { "value": "10", "label": "Writing" }
+                                            { "value": "Lifestyle", "label": "Lifestyle" },
+                                            { "value": "Latest", "label": "Latest Blog" },
+                                            { "value": "Featured", "label": "Featured Blog" },
+                                            { "value": "Tech", "label": "Technology" },
+                                            { "value": "Healthy", "label": "Healthy Lifestyle" },
+                                            { "value": "Fashion", "label": "Fashion" },
+                                            { "value": "Business", "label": "Business" },
+                                            { "value": "Design", "label": "Design" },
+                                            { "value": "Domain Names", "label": "Domain Names" },
+                                            { "value": "Education", "label": "Education" },
+                                            { "value": "Entertainment", "label": "Entertainment" },
+                                            { "value": "Home & Lifestyle", "label": "Home & Lifestyle" },
+                                            { "value": "Marketing", "label": "Marketing" },
+                                            { "value": "Digital Marketing", "label": "Digital Marketing" },
+                                            { "value": "SEO", "label": "SEO" },
+                                            { "value": "Social Media", "label": "Social Media" },
+                                            { "value": "Music", "label": "Music" },
+                                            { "value": "Other", "label": "Other" },
+                                            { "value": "Politics", "label": "Politics" },
+                                            { "value": "Real Estate", "label": "Real Estate" },
+                                            { "value": "Sports", "label": "Sports" },
+                                            { "value": "Technology", "label": "Technology" },
+                                            { "value": "Travel", "label": "Travel" },
+                                            { "value": "Video", "label": "Video" },
+                                            { "value": "Web Hosting", "label": "Web Hosting" },
+                                            { "value": "Web Security", "label": "Web Security" },
+                                            { "value": "Wellness", "label": "Wellness" },
+                                            { "value": "Writing", "label": "Writing" }
                                         ].map((item, index) => {
                                             return (
                                                 <>
-                                                    <option value={item.label}>{item.label}</option>
+                                                    <option value={item.value}>{item.label}</option>
                                                 </>
                                             )
                                         }
