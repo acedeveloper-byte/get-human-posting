b99c9bd5c4a3c9aad723163d783cb7f44e315ef8
Mayank kumar <mayank.kumarace@gmail.com>
2025-04-28 09:49:04 +0530

    adde changes

--- components/HomeSection/FeaturedSection.js
+++ components/HomeSection/FeaturedSection.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HOST } from "@/utils/static";
 
-import Placeholder from 'react-bootstrap/Placeholder';
 
 const DATA = [
     {
@@ -35,9 +34,8 @@ const DATA = [
         type: "placeholder"
     },
 ]
-const FeaturedSection = ({data}) => {
-    var conditionaldata =  data.length !==0 ? data : DATA
-
+const FeaturedSection = ({ data }) => {
+    var conditionaldata = data.length !== 0 ? data : DATA
 
 
     return (
@@ -60,53 +58,50 @@ const FeaturedSection = ({data}) => {
                                             <div class="penci-biggrid-inner default">
                                                 <div class="penci-big-grid-ajax-data">
                                                     <div class="penci-clearfix penci-biggrid-data penci-dblock penci-fixh">
-                                                        {
+                                                        {conditionaldata.slice(0, 5).map((item, idx) => {
+                                                            return (
 
-                                                        conditionaldata.slice(0, 5).map((item, idx) => {
+                                                                <div class={`${idx + 1 === 5 ? `penci-bgitem bgitem-0` : `penci-bgitem bgitem-${idx + 1}`}`}>
+                                                                    <div class="penci-bgitin">
+                                                                        <div class="penci-bgmain">
+                                                                            <div class="pcbg-thumb">
 
-                                                                return (
-
-                                                                    <div class={`${idx + 1 === 5 ? `penci-bgitem bgitem-0` : `penci-bgitem bgitem-${idx + 1}`}`}>
-                                                                        <div class="penci-bgitin">
-                                                                            <div class="penci-bgmain">
-                                                                                <div class="pcbg-thumb">
-
-                                                                                    <div class="pcbg-thumbin">
-                                                                                        <img
-                                                                                            src={ item.type === "placeholder" ? item.image :`${HOST}resources/post/${item.image}`}
-                                                                                            class="penci-lazy penci-image-holder" alt="featured" />
-                                                                                    </div>
+                                                                                <div class="pcbg-thumbin">
+                                                                                    <img
+                                                                                        src={item.type === "placeholder" ? item.image : `${HOST}resources/post/${item.image}`}
+                                                                                        class="penci-lazy penci-image-holder" alt="featured" />
                                                                                 </div>
+                                                                            </div>
 
-                                                                                <div class="pcbg-content">
+                                                                            <div class="pcbg-content">
 
-                                                                                    <div class="pcbg-content-flex">
-                                                                                        <a class="pcbg-bgoverlay active-overlay"
-                                                                                            href={`/${item.url}`}
-                                                                                            title={`${item.title}`}></a>
-                                                                                        <div class="pcbg-content-inner bgcontent-block"> <a
-                                                                                            href={`/${item.url}`}
-                                                                                            title={`${item.title}`}
-                                                                                            class="pcbg-bgoverlaytext item-hover"></a>
-                                                                                            <div class="pcbg-heading item-hover">
-                                                                                                <h3 class="pcbg-title"> <a
-                                                                                                    href={`/${item.url}`}>
-                                                                                                    {item.title} </a>
-                                                                                                </h3>
-                                                                                            </div>
+                                                                                <div class="pcbg-content-flex">
+                                                                                    <a class="pcbg-bgoverlay active-overlay"
+                                                                                        href={`/${item.url}`}
+                                                                                        title={`${item.title}`}></a>
+                                                                                    <div class="pcbg-content-inner bgcontent-block"> <a
+                                                                                        href={`/${item.url}`}
+                                                                                        title={`${item.title}`}
+                                                                                        class="pcbg-bgoverlaytext item-hover"></a>
+                                                                                        <div class="pcbg-heading item-hover">
+                                                                                            <h3 class="pcbg-title"> <a
+                                                                                                href={`/${item.url}`}>
+                                                                                                {item.title} </a>
+                                                                                            </h3>
                                                                                         </div>
                                                                                     </div>
-
-
                                                                                 </div>
 
 
                                                                             </div>
+
+
                                                                         </div>
                                                                     </div>
-                                                                )
-                                                            }
-                                                            )}
+                                                                </div>
+                                                            )
+                                                        }
+                                                        )}
                                                     </div>
                                                 </div>
 
@@ -126,15 +121,3 @@ const FeaturedSection = ({data}) => {
 export default FeaturedSection
 
 
-
-export function CardExample() {
-    return (
-        <div className="d-flex justify-content-around">
-            <p aria-hidden="true">
-                <Placeholder xs={6} />
-            </p>
-
-            <Placeholder.Button xs={4} aria-hidden="true" />
-        </div>
-    );
-}
